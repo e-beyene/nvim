288c0499a81759100a64a29d12ef41a8435bd511
e-beyene <ebeyene@redventures.com>
2024-07-17 08:33:52 -0400

    add deno support

--- src/plugins/nvim-lspconfig.lua
+++ src/plugins/nvim-lspconfig.lua
@@ -88,6 +88,16 @@ if vim.fn.isdirectory(luals_root) == 1 then
 	})
 end
 
-lspconfig.tsserver.setup({ on_attach = on_attach_custom })
 lspconfig.ruby_lsp.setup({ on_attach = on_attach_custom })
 lspconfig.cssls.setup({ on_attach = on_attach_custom })
+
+lspconfig.denols.setup {
+  on_attach = on_attach_custom,
+  root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
+}
+
+lspconfig.tsserver.setup {
+  on_attach = on_attach_custom,
+  root_dir = lspconfig.util.root_pattern("package.json"),
+  single_file_support = false
+}
