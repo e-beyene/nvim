a05e87285703ac4963b774c86dda0517bad71c3e
e-beyene <ebeyene@redventures.com>
2024-07-17 08:34:24 -0400

    remove unused section

--- src/plugins/mini/completion.lua
+++ src/plugins/mini/completion.lua
@@ -3,12 +3,5 @@ completion.setup({
 	lsp_completion = {
 		source_func = "omnifunc",
 		auto_setup = false,
-		process_items = function(items, base)
-			-- Don't show 'Text' and 'Snippet' suggestions
-			items = vim.tbl_filter(function(x)
-				return x.kind ~= 1 and x.kind ~= 15
-			end, items)
-			return require("mini.completion").default_process_items(items, base)
-		end,
 	},
 })
