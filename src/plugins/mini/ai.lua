5aea9494d88f235c35a5a29be5742f696c716efe
e-beyene <ebeyene@redventures.com>
2024-07-30 17:25:30 -0400

    more mini enable

---
+++ src/plugins/mini/ai.lua
@@ -0,0 +1,16 @@
+local ai = require("mini.ai")
+
+ai.setup({
+	n_lines = 500,
+	search_method = "cover_or_nearest",
+	custom_textobjects = {
+		g = function()
+			local from = { line = 1, col = 1 }
+			local to = {
+				line = vim.fn.line("$"),
+				col = math.max(vim.fn.getline("$"):len(), 1),
+			}
+			return { from = from, to = to }
+		end,
+	},
+})
